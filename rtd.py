3cafeb49b60b76829278acf9e62ed9a8c022b8aa
Grafica20162 <djmunera@hotmail.com>
2016-11-05 23:26:18 -0500

    Se agrega modificacion

--- rtd.py
+++ rtd.py
@@ -29,6 +29,16 @@ class botonflotador (pygame.sprite.Sprite):
         self.rect.x = xi
         self.rect.y = yi
 
+class pintaflotador(pygame.sprite.Sprite):
+    def __init__(self,archivo,xi,yi):
+        pygame.sprite.Sprite.__init__(self)
+        self.image  = pygame.image.load(archivo).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.click = False
+        self.rect.x = xi
+        self.rect.y = yi
+        self.id = 0
+
 
 class algas(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -47,8 +57,10 @@ if __name__ == "__main__":
     fondo=pygame.image.load("Fondo.png") #cargo imagen fondo
     shark1=pygame.image.load("shark11.gif") #Cargo sprite de tiburon1
     shark2=pygame.image.load("sharkd.gif") #Cargo sprite de tiburon2
+    Canoa=pygame.image.load("ship.png") #Cargo la canoa
     botonflo = botonflotador("float.png",880,80,"flotador") #Cargo boton flotador
     botonmad = Madera("trunk.png",880,250,"madera") #Cargo boton madera
+
 
 
     #listas de sprites
@@ -56,6 +68,7 @@ if __name__ == "__main__":
     listaalgas=pygame.sprite.Group() #lista donde esta todas las algas
     listabotonflo=pygame.sprite.Group() #lista donde esta todos los flotadores
     listabotonmade=pygame.sprite.Group() #lista donde esta todos las maderas
+    listabloques=pygame.sprite.Group() #lista donde esta todos los botones
     
     #Cargo coordenas del tiburon1
     shark_X=100 
@@ -83,6 +96,9 @@ if __name__ == "__main__":
     listabotonflo.add(botonflo)
     listabotonmade.add(botonmad)
 
+
+
+    contador = 0
     #pantalla.blit(fondo,(0,0)) #con esto coloco el fondo en la pantalla
     #pantalla.blit(shark1,[shark_X,shark_Y]) #Coloco el tiburon1 son las coordenadas en pantalla
     #pantalla.blit(shark2,[shark2_X,shark2_Y]) #Coloco el tiburon2 son las coordenadas en pantalla
@@ -96,6 +112,54 @@ fin = False
 while not fin:
 
     for event in pygame.event.get():
+    	if event.type == pygame.MOUSEBUTTONDOWN:
+                for b in listabotonflo:
+                	if b.rect.collidepoint(event.pos):
+                		flotador = pintaflotador("float.png",750,0)
+                		#print "pase"
+                        flotador.id = contador
+                        contador += 1
+                        print contador
+                        col = True
+
+                        while col:
+                            col = False
+                            colision = pygame.sprite.spritecollide(flotador,listabloques,False)
+                            for bl in colision:
+                                if flotador.id != bl.id:
+                                    flotador.rect.left = bl.rect.right
+                                    col = True
+
+                        listabloques.add(flotador)
+                        listatodos.add(flotador)
+
+                for bloque in listabloques:
+                    if bloque.rect.collidepoint(event.pos):
+                        bloque.update(pantalla)
+                        bloque.click = True
+                    elif event.type == pygame.MOUSEBUTTONUP:
+                    	for bloque in listabloques:
+	                        bloque.update(pantalla)
+	                        bloque.click = False
+	                        col = True
+
+	                        while col:
+	                            col = False
+	                            colision =pygame.sprite.spritecollide(bloque,listabloques,False)
+	                            for e in colision:
+	                                if bloque.id != e.id:
+	                                    bloque.rect.left = e.rect.right
+	                                    col = True
+
+
+
+
+
+
+
+
+
+
         if event.type == pygame.QUIT:
             fin = True
 
@@ -122,6 +186,7 @@ while not fin:
     pantalla.blit(fondo,(0,0))
     pantalla.blit(shark1,[shark_X,shark_Y])
     pantalla.blit(shark2,[shark2_X,shark2_Y])
+    pantalla.blit(Canoa,[400,0])
     listatodos.update()
     listatodos.draw(pantalla)
     pygame.display.flip()
