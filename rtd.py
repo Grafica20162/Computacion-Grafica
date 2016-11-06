3658b0f5e6d63bd0cb16b21ae94e5550f58d51de
Grafica20162 <djmunera@hotmail.com>
2016-11-06 01:35:57 -0500

    Se agrega modificacion

--- rtd.py
+++ rtd.py
@@ -29,6 +29,8 @@ class botonflotador (pygame.sprite.Sprite):
         self.rect.x = xi
         self.rect.y = yi
 
+
+
 class pintaflotador(pygame.sprite.Sprite):
     def __init__(self,archivo,xi,yi):
         pygame.sprite.Sprite.__init__(self)
@@ -38,6 +40,11 @@ class pintaflotador(pygame.sprite.Sprite):
         self.rect.x = xi
         self.rect.y = yi
         self.id = 0
+
+    def update(self,surface):
+        if self.click :
+            self.rect.center = pygame.mouse.get_pos()
+        surface.blit(self.image,self.rect)
 
 
 class algas(pygame.sprite.Sprite):
@@ -59,7 +66,7 @@ if __name__ == "__main__":
     shark2=pygame.image.load("sharkd.gif") #Cargo sprite de tiburon2
     Canoa=pygame.image.load("ship.png") #Cargo la canoa
     botonflo = botonflotador("float.png",880,80,"flotador") #Cargo boton flotador
-    botonmad = Madera("trunk.png",880,250,"madera") #Cargo boton madera
+    botonmad = Madera("trunk.png",900,280,"madera") #Cargo boton madera
 
 
 
@@ -82,23 +89,25 @@ if __name__ == "__main__":
     flag=False
     flag2=False
 
-    #Creo las algas y tiburones con sus posiciones
-    a=algas(200,445)
-    b=algas(300,145)
-    c=algas(500,245)
-    d=algas(80,145)
-    f=algas(700,300)
-
+    #Creo algas aleatoriamente dentro del mar
+    for i in range(30):
+        x=random.randrange(ANCHO-20)
+        y=random.randrange(ALTO-20)
+        if x < 700:
+            if y < 400 and y > 50:
+                e=algas(x,y)
+                listatodos.add(e)
+                listaalgas.add(e)
 
     #Agrego los sprites a las listas
-    listatodos.add(a,b,c,d,f,botonflo,botonmad)
-    listaalgas.add(a,b,c,d,f)
+    listatodos.add(botonflo,botonmad)
     listabotonflo.add(botonflo)
     listabotonmade.add(botonmad)
 
 
 
     contador = 0
+    contador2 = 0
     #pantalla.blit(fondo,(0,0)) #con esto coloco el fondo en la pantalla
     #pantalla.blit(shark1,[shark_X,shark_Y]) #Coloco el tiburon1 son las coordenadas en pantalla
     #pantalla.blit(shark2,[shark2_X,shark2_Y]) #Coloco el tiburon2 son las coordenadas en pantalla
@@ -112,54 +121,83 @@ fin = False
 while not fin:
 
     for event in pygame.event.get():
-    	if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
                 for b in listabotonflo:
-                	if b.rect.collidepoint(event.pos):
-                		flotador = pintaflotador("float.png",750,0)
-                		#print "pase"
-                        flotador.id = contador
-                        contador += 1
-                        print contador
-                        col = True
+                    if (contador<=2):
+                        if b.rect.collidepoint(event.pos):
+                            flotador = pintaflotador("float.png",500,0)
+                            #print "pase"
+                            flotador.id = contador
+                            contador += 1
+                            print contador
+                            col = True
 
-                        while col:
-                            col = False
-                            colision = pygame.sprite.spritecollide(flotador,listabloques,False)
-                            for bl in colision:
-                                if flotador.id != bl.id:
-                                    flotador.rect.left = bl.rect.right
-                                    col = True
+                            while col:
+                                col = False
+                                colision = pygame.sprite.spritecollide(flotador,listabloques,False)
+                                for bl in colision:
+                                    if flotador.id != bl.id:
+                                        flotador.rect.left = bl.rect.right
+                                        col = True
 
-                        listabloques.add(flotador)
-                        listatodos.add(flotador)
+                            listabloques.add(flotador)
+                            listatodos.add(flotador)
 
                 for bloque in listabloques:
                     if bloque.rect.collidepoint(event.pos):
                         bloque.update(pantalla)
                         bloque.click = True
-                    elif event.type == pygame.MOUSEBUTTONUP:
-                    	for bloque in listabloques:
-	                        bloque.update(pantalla)
-	                        bloque.click = False
-	                        col = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for bloque in listabloques:
+                    bloque.update(pantalla)
+                    bloque.click = False
+                    col = True
+                    while col:
+                        col = False
+                        colision =pygame.sprite.spritecollide(bloque,listabloques,False)
+                        for e in colision:
+                            if bloque.id != e.id:
+                                bloque.rect.left = e.rect.right
+                                col = True
+        #Madera
 
-	                        while col:
-	                            col = False
-	                            colision =pygame.sprite.spritecollide(bloque,listabloques,False)
-	                            for e in colision:
-	                                if bloque.id != e.id:
-	                                    bloque.rect.left = e.rect.right
-	                                    col = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                for m in listabotonmade:
+                    if (contador2<=2):
+                        if m.rect.collidepoint(event.pos):
+                            maderaa = pintaflotador("trunk.png",200,0)
+                            #print "pase"
+                            maderaa.id = contador
+                            contador2 += 1
+                            print contador2
+                            col = True
 
+                            while col:
+                                col = False
+                                colision = pygame.sprite.spritecollide(maderaa,listabloques,False)
+                                for bl in colision:
+                                    if maderaa.id != bl.id:
+                                        maderaa.rect.left = bl.rect.right
+                                        col = True
 
-
-
-
-
-
-
-
-
+                            listabloques.add(maderaa)
+                            listatodos.add(maderaa)
+                for bloquem in listabloques:
+                    if bloquem.rect.collidepoint(event.pos):
+                        bloquem.update(pantalla)
+                        bloquem.click = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for bloquem in listabloques:
+                    bloquem.update(pantalla)
+                    bloquem.click = False
+                    col = True
+                    while col:
+                        col = False
+                        colision =pygame.sprite.spritecollide(bloque,listabloques,False)
+                        for e in colision:
+                            if bloque.id != e.id:
+                                bloque.rect.left = e.rect.right
+                                col = True           
         if event.type == pygame.QUIT:
             fin = True
 
@@ -184,10 +222,12 @@ while not fin:
       shark2_X+=2
 
     pantalla.blit(fondo,(0,0))
+    pantalla.blit(Canoa,[400,0])
+    
+    listatodos.draw(pantalla)
+    pygame.display.update()
+    listatodos.update(pantalla)
     pantalla.blit(shark1,[shark_X,shark_Y])
     pantalla.blit(shark2,[shark2_X,shark2_Y])
-    pantalla.blit(Canoa,[400,0])
-    listatodos.update()
-    listatodos.draw(pantalla)
     pygame.display.flip()
 reloj.tick(60)
