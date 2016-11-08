55fe2460b209a206374c7dfab17b2db03a80e9ca
Grafica20162 <djmunera@hotmail.com>
2016-11-07 22:06:34 -0500

    Se agrego modificacion

--- rtd.py
+++ rtd.py
@@ -11,6 +11,32 @@ ROJO=[255,0,0]
 azul=[0,255,255]
 GRIN=[0,200,0]
 
+#Clase donde carga los tiburones
+class shark11(pygame.sprite.Sprite):
+    def __init__(self,archivo,xi,yi,nombre):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(archivo).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.nombre = nombre
+        self.rect.x = xi
+        self.rect.y = yi
+
+        self.vida=20 #Definimos la longitud de vida
+        #self.sonido2=pygame.mixer.Sound("comidabuena.wav")
+        self.sonido1=pygame.mixer.Sound("mordida.wav")
+        self.golpe=True
+
+
+    #con esto le vamos restando cada vez que golpe y se le resta de a 5
+    def choque(self):
+        self.vida-=20
+        print "Tu vida es: "
+        print self.vida
+        if  self.golpe:
+            self.sonido1.play()
+
+
+
 #Clase Jugador principal
 class Jugador(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -21,7 +47,7 @@ class Jugador(pygame.sprite.Sprite):
         self.rect.y=y
         self.var_x=0
         self.var_y=0
-        self.vida=20 #Definimos la longitud de vida
+        self.vida=400 #Definimos la longitud de vida
         #self.sonido2=pygame.mixer.Sound("comidabuena.wav")
         self.sonido1=pygame.mixer.Sound("mordida.wav")
         self.golpe=True
@@ -74,6 +100,8 @@ class Madera (pygame.sprite.Sprite):
         self.nombre = nombre
         self.rect.x = xi
         self.rect.y = yi
+        self.var_x=100
+        self.var_y=100
 
 #Clase donde carga el boton flotador la madera cada vez
 class botonflotador (pygame.sprite.Sprite):
@@ -118,8 +146,8 @@ if __name__ == "__main__":
     pygame.init()
     pantalla = pygame.display.set_mode((ANCHO, ALTO))
     fondo=pygame.image.load("Fondo.png") #cargo imagen fondo
-    shark1=pygame.image.load("shark11.gif") #Cargo sprite de tiburon1
-    shark2=pygame.image.load("sharkd.gif") #Cargo sprite de tiburon2
+    shark1= shark11("shark11.gif",100,100,"Tiburon1") #Cargo sprite de tiburon1
+    shark2= shark11("sharkd.gif",800,400,"Tiburon2") #Cargo sprite de tiburon2
     Canoa=pygame.image.load("ship.png") #Cargo la canoa
     botonflo = botonflotador("float.png",920,80,"flotador") #Cargo boton flotador
     botonmad = Madera("trunk.png",930,280,"madera") #Cargo boton madera
@@ -133,16 +161,9 @@ if __name__ == "__main__":
     listabotonflo=pygame.sprite.Group() #lista donde esta todos los flotadores
     listabotonmade=pygame.sprite.Group() #lista donde esta todos las maderas
     listabloques=pygame.sprite.Group() #lista donde esta todos los botones
+    listatiburones=pygame.sprite.Group() #lista donde esta tdos los tiburones
     #l_col=pygame.sprite.spritecollide(jp,shark1,shark2,True) #lista de colociones con tiburones
     
-    #Cargo coordenas del tiburon1
-    shark_X=100 
-    shark_Y=100
-
-    #Cargo coordenadas del tiburon2
-    shark2_X=800 
-    shark2_Y=400
-
     #Banderas de partida de los tiburones
     flag=False
     flag2=False
@@ -158,9 +179,10 @@ if __name__ == "__main__":
                 listaalgas.add(e)
 
     #Agrego los sprites a las listas
-    listatodos.add(botonflo,botonmad,jp)
+    listatodos.add(botonflo,botonmad,jp,shark1,shark2)
     listabotonflo.add(botonflo)
     listabotonmade.add(botonmad)
+    listatiburones.add(shark1,shark2)
 
 
 
@@ -180,17 +202,11 @@ while not fin:
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    jp.var_x=5
-                    jp.var_y=0
-                if event.key==pygame.K_LEFT: #solo se mueve con la tecla de la Izquierda
-                    jp.var_x=-5
-                    jp.var_y=0
                 if event.key==pygame.K_DOWN: #solo se mueve con la tecla de abajo
-                    jp.var_y+=5
+                    jp.var_y=4
                     jp.var_x=0
                 if event.key==pygame.K_UP: #solo se mueve con la tecla de ARRIBA
-                    jp.var_y+=-5
+                    jp.var_y=-4
                     jp.var_x=0
                 if event.key == pygame.K_s: # con la tecla s hace stop
                     jp.var_x=0
@@ -202,6 +218,9 @@ while not fin:
                     if (contador<=2):
                         if b.rect.collidepoint(event.pos):
                             flotador = pintaflotador("float.png",600,0)
+                            sonido=pygame.mixer.Sound("seleccion.wav")
+                            sonido.play()
+                            #listabotonflo.add(flotador)
                             #print "pase"
                             flotador.id = contador
                             contador += 1
@@ -242,6 +261,9 @@ while not fin:
                     if (contador2<=2):
                         if m.rect.collidepoint(event.pos):
                             maderaa = pintaflotador("trunk.png",200,0)
+                            sonido2=pygame.mixer.Sound("seleccion.wav")
+                            sonido2.play()
+                            #listabotonmade.add(maderaa)
                             #print "pase"
                             maderaa.id = contador2
                             contador2 += 1
@@ -281,29 +303,44 @@ while not fin:
     margen()
 
     #MOVIMIENTO DE IZQUIERDA A DERECHA EN LA VENTANA DE TIBURON1
-    if (shark_X==0):
+    if (shark1.rect.x==0):
      flag = False
-    if(shark_X==800):
+    if(shark1.rect.x==800):
      flag=True
     if flag:
-      shark_X-=2
+      shark1.rect.x-=2
     else:
-      shark_X+=2
+      shark1.rect.x+=2
 
      #MOVIMIENTO DE DERECHA A IZQUIERDA EN LA VENTANA DE TIBURON2
-    if (shark2_X==0):
+    if (shark2.rect.x==0):
      flag2 = False
-    if(shark2_X==800):
+    if(shark2.rect.x==800):
      flag2=True
     if flag2:
-      shark2_X-=2
+      shark2.rect.x-=2
     else:
-      shark2_X+=2
+      shark2.rect.x+=2
 
-    #l_col=pygame.sprite.spritecollide(shark1,botonflo,True) #lista de colociones con tiburones
+    l_col=pygame.sprite.spritecollide(shark1,listabloques,True) #lista de colociones de tiburon 1 con flotadores y maderas
+    l_col2=pygame.sprite.spritecollide(shark2,listabloques,True) #lista de colociones de tiburon 2 con flotadores y maderas
+    l_coljugador=pygame.sprite.spritecollide(jp,listatiburones,False) #lista de colociones de tiburones con jugador
+    l_colmad=pygame.sprite.spritecollide(jp,listatiburones,False) #lista de colociones de tiburones con madera
+
     #cada que se golpe le voy quitando vida y suena
-    #for en in l_col:
-     #   jp.choque()
+    for en in l_col:
+        shark1.choque()
+        if(listabloques==[]): #PARA SABER QUE SE ACABARON LOS FLOTADORES Y MADERAS Y ASI ACABAR JUEGO
+           print "perdio"
+         #   fin=True
+    for ennn in l_col2:
+        shark2.choque()
+
+    for enn in l_coljugador:
+        jp.choque()
+        if(jp.vida==0):
+            print "Perdio por bobo"
+            fin=True
 
     pantalla.blit(fondo,(0,0))
     pantalla.blit(Canoa,[400,0]) #Pinto canoa en esa posicion
@@ -311,7 +348,7 @@ while not fin:
     listatodos.draw(pantalla)
     pygame.display.update()
     listatodos.update(pantalla)
-    pantalla.blit(shark1,[shark_X,shark_Y])
-    pantalla.blit(shark2,[shark2_X,shark2_Y])
+    #pantalla.blit(shark1,[shark_X,shark_Y])
+    #pantalla.blit(shark2,[shark2_X,shark2_Y])
     pygame.display.flip()
-reloj.tick(60)
+    reloj.tick(40)
